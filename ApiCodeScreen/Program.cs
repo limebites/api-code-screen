a000627137b41253f76c52a7bc48ff4c504f9f64
David Arellano <david.arellano@priorauthnow.com>
2022-02-24 16:58:42 -0500

    migrated project to dotnet core 3.1

--- ApiCodeScreen/Program.cs
+++ ApiCodeScreen/Program.cs
@@ -1,30 +1,37 @@
-﻿
-var dataSourceUrl = "https://k7o2mgxtv8.execute-api.us-east-1.amazonaws.com/public/manufacturers";
-var carNamesPostUrl = "https://k7o2mgxtv8.execute-api.us-east-1.amazonaws.com/public/names";
-var carColorsPostUrl = "https://k7o2mgxtv8.execute-api.us-east-1.amazonaws.com/public/colors";
-var httpClient = new HttpClient();
+﻿using System.Collections.Generic;
+using System.Net.Http;
 
-// TODO: use httpClient to GET Manufacturer data from the dataSourceUrl
-
-// TODO: use httpClient to POST a list of unique car names offered by Ford, sorted in alphabetical order, to the carNamesPostUrl
-// the POST body should be a JSON object { "names": [...] }
-
-// TODO: use httpClient to POST a list of unique unique car colors offered by Chevy, sorted in alphabetical order, to the carColorsPostUrl
-// the POST body should be a JSON object { "colors": [...] }
-
-class Root
+namespace ApiCodeScreen
 {
-    public List<Manufacturer> Manufacturers { get; set; } = null!;
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataSourceUrl = "https://k7o2mgxtv8.execute-api.us-east-1.amazonaws.com/public/manufacturers";
+            var carNamesPostUrl = "https://k7o2mgxtv8.execute-api.us-east-1.amazonaws.com/public/names";
+            var carColorsPostUrl = "https://k7o2mgxtv8.execute-api.us-east-1.amazonaws.com/public/colors";
+            var httpClient = new HttpClient();
 
-class Manufacturer
-{
-    public string Name { get; set; } = null!;
-    public List<Car> Cars { get; set; } = new();
-}
+            /**
+             * TODO
+             */
+        }
+    }
 
-class Car
-{
-    public string Name { get; set; } = null!;
-    public List<string> Colors { get; set; } = new();
+    class Root
+    {
+        public List<Manufacturer> Manufacturers { get; set; } = null!;
+    }
+
+    class Manufacturer
+    {
+        public string Name { get; set; } = null!;
+        public List<Car> Cars { get; set; } = new List<Car>();
+    }
+
+    class Car
+    {
+        public string Name { get; set; } = null!;
+        public List<string> Colors { get; set; } = new List<string>();
+    }
 }
